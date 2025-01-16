57d4955a76240bc58b978f16dd08a52083c76e0d
Michael Grundt <mgrundt@web.de>
2025-01-16 10:47:48 +0100

    Cleaning of Files

--- program/routes.py
+++ program/routes.py
@@ -1,7 +1,13 @@
 from program import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 # Funtionnames gleich der Route: Best Practice
 def index():
-    return 'Hello Word!'
+    return render_template("index.html")
+
+@app.route('/100Days') #Keine Dateiendung im Dekorator, das funktioniert hier nicht
+# Funtionnames gleich der Route: Best Practice, Keine Zahl am Anfang, daner das "p"
+def p100Days():
+    return render_template("100Days.html")
